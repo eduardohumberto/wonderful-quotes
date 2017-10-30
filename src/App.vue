040f4f5f7bbbb54b6e029d9596e59c6c5dc57dda
Eduardo humberto <eduardo.humberto1992@gmail.com>
2017-10-30 14:14:46 -0200

    creating components 2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
-        
+        data:function(){
+            return {
+                quotes:[
+                    'Just a quote to see something'
+                ],
+                maxQuotes:10
+            }
+        },
+        components: {
+            appQuoteGrid:QuoteGrid,
+            appNewQuote:NewQuote
+        },
+        methods:{
+           newQuote(quote){
+               this.quotes.push(quote);
+           }
+        }
     }
 </script>
 
